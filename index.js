4ee933b4d4e552f9074f0c726e8ffd914b765249
Fernando.Zorek <fernando.zorek@icolabora.com>
2024-05-04 18:25:21 -0300

    Added chokidar to watch path files

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const ResumeProcessor = require('./resumeProcessor');
-const DatabaseManager = require('./bd/mongodb/databaseManager');
-const PdfParser = require('./pdfParser'); 
-const AiEnhancer = require('./ai/aiEnhancer'); 
 
-const pdfFilePath = './files/resume.pdf'; // Replace with actual PDF path
+const lastFile = require('./lastFile')
 
 async function main() {
-  const pdfParser = new PdfParser();
-  const aiEnhancer = new AiEnhancer();
-  const resumeProcessor = new ResumeProcessor(pdfParser, aiEnhancer);
-  const normalizedData = await resumeProcessor.processResume(pdfFilePath);
-  const databaseManager = new DatabaseManager();
-  await databaseManager.create();
-  await databaseManager.save(normalizedData);
+  await lastFile();
 }
 
 main();
---
+++ lastFile.js
@@ -0,0 +1,53 @@
+
+const ResumeProcessor = require('./resumeProcessor');
+const DatabaseManager = require('./bd/mongodb/databaseManager');
+const PdfParser = require('./pdfParser'); 
+const AiEnhancer = require('./ai/aiEnhancer'); 
+const fs = require('fs')
+const fsPromises = require('fs/promises')
+const chokidar = require('chokidar')
+
+
+module.exports = (async () => { 
+
+  console.log('Searching for files *.pdf...')
+  const PATH_FILES = './files'
+  const sendToQueue = async (fileBuffer, filename) => {
+    const pdfFilePath = './' + filename;
+    console.log('Find file: ', pdfFilePath)
+    const pdfParser = new PdfParser();
+    const aiEnhancer = new AiEnhancer();
+    const resumeProcessor = new ResumeProcessor(pdfParser, aiEnhancer);
+    const normalizedData = await resumeProcessor.processResume(pdfFilePath);
+    const databaseManager = new DatabaseManager();
+    await databaseManager.create();
+    await databaseManager.save(normalizedData);
+    const renameFile = fsPromises.rename;
+    await renameFile(pdfFilePath, pdfFilePath + '.old');
+  }
+
+  const watchedFolderLoteFiles = chokidar.watch(PATH_FILES, {
+    ignoreInitial: false,
+    ignored: '*.old'
+  })
+
+  const filterFileListByExtension = (files, extension) => {
+    const { extname, basename } = require('path')
+    return files.filter(f => extname(basename(f)) === extension)
+  }
+
+  watchedFolderLoteFiles.on('ready', () => console.log(`Waiting for files in volume ${PATH_FILES}`))
+
+  watchedFolderLoteFiles.on('add', (path) => {
+    if (filterFileListByExtension([path], '.pdf').length > 0) {
+      return sendToQueue(fs.readFileSync(path), path)
+    }
+  })
+
+  watchedFolderLoteFiles.on('error', (error) => {
+    logger.fatal(error)
+    process.exit(1)
+  })
+
+  console.log('...Finished AI Process!')
+  })
